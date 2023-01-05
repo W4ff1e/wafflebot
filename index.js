466348515e64c04580e6191a1c5e7988c80d736f
W4ff1e <wafflegamingyt@gmail.com>
2023-01-05 11:55:12 -0500

    Started Security Fixes and other Misc. Changes

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 // Require Dependencies
 
-// Command Handling
+// Command Intialization
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+// Initialize the Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (Once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
-
+// More Command Intialization
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
@@ -33,6 +26,17 @@ for (const file of commandFiles) {
 	}
 }
 
+
+// When the client is ready, run this code (Once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+// Command Listener
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
