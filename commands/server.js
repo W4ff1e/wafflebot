7336c1f5cef54712492c70704eea2adb4dc7a7a0
W4ff1e <waffle@projectwaffle.net>
2023-01-06 13:02:42 -0500

    Updated formatting and licensing

--- commands/server.js
+++ commands/server.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Provides information about the server.'),
-	async execute(interaction) {
-		// interaction.guild is the object representing the server in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.\nThe server is located on the shard: ${interaction.guild.shard}`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Provides information about the server.'),
+    async execute(interaction) {
+        // interaction.guild is the object representing the server in which the command was run
+        await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.\nThe server is located on the shard: ${interaction.guild.shard}`);
+    },
 };
