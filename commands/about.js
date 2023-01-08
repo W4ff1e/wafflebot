cb187a77c211f288ca30a76b3960dfd221051dea
W4ff1e <waffle@projectwaffle.net>
2023-01-07 23:44:02 -0500

    Added help command and hyperlinks

--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
-        await interaction.reply(`I was made by [BLD] Waffle#1816 and licensed under Apache-2.0 for public use!\nYou can get help and send suggestions in our development/support server found here:\n${developmentGuildInvite}`);
+        await interaction.reply(`I was made by [BLD] Waffle#1816 and licensed under Apache-2.0 for public use!\nYou can get help and send suggestions in our development/support server found [here](${developmentGuildInvite})`);
     },
 };
 
---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { commandHelpWiki } = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Provides a link to the command overview wiki.'),
+    async execute(interaction) {
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.reply(`[Here is the link to the Github Wiki for commands](${commandHelpWiki})`);
+    },
+};
+
